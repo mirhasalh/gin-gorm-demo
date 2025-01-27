1e7c8bea70a2ac8730e112a6c1530e303adc2106
Irhas <muhammadirhasalh@gmail.com>
2025-01-27 21:52:23 +0700

    feat(posts): completed crud

--- controllers/postsController.go
+++ controllers/postsController.go
@@ -1,12 +1,89 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/mirhasalh/gin-gorm-demo/initializers"
+	"github.com/mirhasalh/gin-gorm-demo/models"
+)
 
 func PostsCreate(c *gin.Context) {
 	// Get data from request body
+	var body struct {
+		Body  string `json:"body" binding:"required"`
+		Title string `json:"title" binding:"required"`
+	}
+
+	c.Bind(&body)
 
 	// Create a new post
+	post := models.Post{Title: body.Title, Body: body.Body}
+	result := initializers.DB.Create(&post)
+
+	if result.Error != nil {
+		c.Status(400)
+		return
+	}
 
 	// Return the post
-	c.JSON(200, gin.H{ "message": "Post created" })
+	c.JSON(200, gin.H{
+		"post": post,
+	})
+}
+
+func PostsIndex(c *gin.Context) {
+	var posts []models.Post
+	initializers.DB.Find(&posts)
+
+	c.JSON(200, gin.H{
+		"posts": posts,
+	})
+}
+
+func PostsShow(c *gin.Context) {
+	// Get id from URL
+	id := c.Param("id")
+
+	var post models.Post
+	initializers.DB.First(&post, id)
+
+	c.JSON(200, gin.H{
+		"post": post,
+	})
+}
+
+func PostsUpdate(c *gin.Context) {
+	// Get id from URL
+	id := c.Param("id")
+
+	// Get data from request body
+	var body struct {
+		Body  string `json:"body" binding:"required"`
+		Title string `json:"title" binding:"required"`
+	}
+
+	c.Bind(&body)
+
+	// Update the post
+	var post models.Post
+	initializers.DB.First(&post, id)
+
+	post.Title = body.Title
+	post.Body = body.Body
+	initializers.DB.Save(&post)
+
+	c.JSON(200, gin.H{
+		"post": post,
+	})
+}
+
+func PostsDelete(c *gin.Context) {
+	// Get id from URL
+	id := c.Param("id")
+
+	// Delete the post
+	var post models.Post
+	initializers.DB.First(&post, id)
+	initializers.DB.Delete(&post)
+
+	c.Status(204)
 }
--- main.go
+++ main.go
@@ -14,7 +14,11 @@ func init() {
 func main() {
 	r := gin.Default()
 
-	r.GET("/posts", controllers.PostsCreate)
+	r.POST("/posts", controllers.PostsCreate)
+	r.GET("/posts", controllers.PostsIndex)
+	r.GET("/posts/:id", controllers.PostsShow)
+	r.PUT("/posts/:id", controllers.PostsUpdate)
+	r.DELETE("/posts/:id", controllers.PostsDelete)
 
 	r.Run()
 }
